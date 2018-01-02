37b39ee35834154514fd13a2e2e89c86a138efd3
Bojan Malinic <bojan.malinic@outlook.com>
2018-01-02 16:31:08 +0100

    Added unit test project

---
+++ StegoAppTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StegoAppTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StegoAppTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f006b010-f057-460d-b36f-05fb42e9a1ca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StegoAppTests/UnitTest1.cs
@@ -0,0 +1,109 @@
+﻿using StegoApp;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Security.Cryptography.X509Certificates;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using System.Security.Cryptography;
+using System.IO;
+using System.Linq;
+
+namespace StegoApp.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void MessageMethodTest()
+        {
+
+            User.LoadUsers();
+            User user = User.AllUsers["mm"];
+            string message = "My message";
+
+            string xmlStr = Message.MakeXml(user, "My message");
+            (string recvMessage, User recvUser, DateTime dt) = Message.ParseXml(xmlStr);
+
+
+            Assert.AreEqual(message, recvMessage);
+            Assert.AreEqual(user, recvUser);
+
+        }
+
+        [TestMethod()]
+        public void TestCertFindFound()
+        {
+
+            User.LoadUsers();
+            User user = User.AllUsers["mm"];
+
+            Assert.IsTrue(CryptoService.FindCertificate(user) != null);
+
+        }
+
+
+        [TestMethod()]
+        public void TestSymmetricEncryption()
+        {
+
+            var symmKey = CryptoService.GenerateSymmetricKey();
+            var iV = CryptoService.GenerateIV();
+
+            string data = "My Message";
+
+            var encData = CryptoService.EncryptData(data, symmKey, iV);
+            var decData = CryptoService.DecryptData(encData, symmKey, iV);
+
+            Assert.AreEqual(data, decData);
+
+        }
+
+        [TestMethod()]
+        public void TestPemReader()
+        {
+
+            var rsaObject = CryptoService.ReadPemFile("MM.pem");
+
+        }
+
+        [TestMethod()]
+        public void TestDigitalEnvelope()
+        {
+
+            User.LoadUsers();
+            User user = User.AllUsers["mm"];
+
+            var symmKey = CryptoService.GenerateSymmetricKey();
+            var iV = CryptoService.GenerateIV();
+
+            var cert = CryptoService.FindCertificate(user);
+            var encData = CryptoService.EncryptSymmetricData(symmKey, iV, cert.GetRSAPublicKey());
+
+            var rsaObject = CryptoService.ReadPemFile("MM.pem");
+            var decData = CryptoService.DecryptSymmetricData(encData, rsaObject);
+
+            Assert.IsTrue(Enumerable.SequenceEqual(symmKey, decData.Item1));
+            Assert.IsTrue(Enumerable.SequenceEqual(iV, decData.Item2));
+
+        }
+
+        [TestMethod()]
+        public void TestDigitalSignature()
+        {
+
+            User.LoadUsers();
+            User user = User.AllUsers["mm"];
+
+            var cert = CryptoService.FindCertificate(user);
+            var rsaObject = CryptoService.ReadPemFile("MM.pem");
+
+            var data = new byte[] { 1, 2, 3, 4, 5, 6 };
+            var signature = CryptoService.SignData(data, rsaObject);
+
+            Assert.IsTrue(CryptoService.VerifyData(data, signature, cert.GetRSAPublicKey()));
+
+        }
+
+    }
+}
