ad7c80a04dc52d18d35821cd89eed0e94d7e742d
Bojan Malinic <bojan.malinic@outlook.com>
2018-01-06 22:36:38 +0100

    Add class "UnreadList"
    New class represents list of unread messages
    for user.

---
+++ StegoApp/UnreadList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace StegoApp
+{
+    class UnreadList
+    {
+
+        // Key is path to image, Value is number of 
+        // times message is written to image
+        Dictionary<string, int> imageList;
+
+        public UnreadList(string filename)
+        {
+
+            var imagePaths = File.ReadAllLines(filename);
+            imageList = new Dictionary<string, int>();
+
+            foreach (var imagePath in imagePaths)
+            {
+
+                if (! imageList.ContainsKey(imagePath))
+                    imageList.Add(imagePath, 1);
+                else
+                    ++imageList[imagePath];
+
+            }
+
+        }
+
+        public IReadOnlyDictionary<string, int> Images
+        {
+
+            get
+            {
+                return imageList;
+            }
+
+        }
+
+        public void Add(string path)
+        {
+
+            if (! imageList.ContainsKey(path))
+                imageList.Add(path, 1);
+            else
+                ++imageList[path];
+
+        }
+
+        public void Write(string filename)
+        {
+
+            using (StreamWriter file = new StreamWriter(filename))
+            {
+                foreach (var imagePath in imageList.Keys)
+                {
+
+                    int count = imageList[imagePath];
+
+                    for(int i = 0; i < count; ++i)
+                        file.WriteLine(imagePath);
+
+                }
+                
+            }
+
+        }
+         
+    }
+}
