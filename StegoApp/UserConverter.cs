612da554f45381443a9dc517c72d6a6fb2912d20
Bojan Malinic <bojan.malinic@outlook.com>
2018-01-08 15:56:28 +0100

    Added user converter class
    To be used in user select window

---
+++ StegoApp/UserConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace StegoApp
+{
+
+    [ValueConversion(typeof(User), typeof(string))]
+    class UserConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            var user = (User)value;
+
+            return $"{user.FullName} ({user.Nickname})";
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
