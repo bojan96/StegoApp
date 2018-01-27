387428d0d3228cafae401b6d809ed17a87e37a70
Bojan Malinic <bojan.malinic@outlook.com>
2018-01-27 14:33:42 +0100

    Added utility class

---
+++ StegoApp/Utility.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StegoApp
+{
+    public static class Utility
+    {
+
+        public static byte[] CombineByteArrays(byte[] array1, byte[] array2)
+        {
+
+            var dstArray = new byte[sizeof(int) + array1.Length + array2.Length];
+            BitConverter.GetBytes(array1.Length).CopyTo(dstArray, 0);
+            array1.CopyTo(dstArray, sizeof(int));
+            array2.CopyTo(dstArray, sizeof(int) + array1.Length);
+
+            return dstArray;
+        }
+
+        public static (byte[], byte[]) SplitArray(byte[] array)
+        {
+
+            int array1Len = BitConverter.ToInt32(array.Take(sizeof(int)).ToArray(), 0);
+            int array2Len = array.Length - sizeof(int) - array1Len;
+            var array1 = new byte[array1Len];
+            var array2 = new byte[array2Len];
+            Array.Copy(array, sizeof(int), array1, 0, array1Len);
+            Array.Copy(array, sizeof(int) + array1Len, array2, 0, array2Len);
+
+            return (array1, array2);
+
+        }
+
+    }
+}
